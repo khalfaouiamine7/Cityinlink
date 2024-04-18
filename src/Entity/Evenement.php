91e26d10ea6ff3ea9b8579fd9ffc92913806f849
lachrefff <99406526+lachrefff@users.noreply.github.com>
2024-04-18 10:16:18 +0100

    Commit

---
+++ src/Entity/Evenement.php
@@ -0,0 +1,126 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Evenement
+ *
+ * @ORM\Table(name="evenement")
+ * @ORM\Entity
+ */
+class Evenement
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer", nullable=false)
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="IDENTITY")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="nom", type="string", length=255, nullable=false)
+     */
+    private $nom;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="description", type="text", length=0, nullable=false)
+     */
+    private $description;
+
+    /**
+     * @var \DateTime|null
+     *
+     * @ORM\Column(name="date_e", type="date", nullable=true)
+     */
+    private $dateE;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="image", type="string", length=255, nullable=false)
+     */
+    private $image;
+
+
+    /**
+     * @ORM\ManyToOne(targetEntity="App\Entity\Categorie", inversedBy="evenements", cascade={"remove"})
+     * @ORM\JoinColumn(nullable=false)
+     */
+    private $categorie;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNom(): ?string
+    {
+        return $this->nom;
+    }
+
+    public function setNom(string $nom): static
+    {
+        $this->nom = $nom;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(string $description): static
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getDateE(): ?\DateTimeInterface
+    {
+        return $this->dateE;
+    }
+
+    public function setDateE(?\DateTimeInterface $dateE): static
+    {
+        $this->dateE = $dateE;
+
+        return $this;
+    }
+
+    public function getImage(): ?string
+    {
+        return $this->image;
+    }
+
+    public function setImage(string $image): static
+    {
+        $this->image = $image;
+
+        return $this;
+    }
+
+    public function getCategorie(): ?Categorie
+    {
+        return $this->categorie;
+    }
+
+    public function setCategorie(?Categorie $categorie): self
+    {
+        $this->categorie = $categorie;
+
+        return $this;
+    }
+
+
+}
---
+++ src/Form/EvenementType.php
@@ -0,0 +1,60 @@
+<?php
+namespace App\Form;
+
+use App\Entity\Evenement;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use App\Entity\Categorie; // Assuming Categorie is an entity in your application
+use Symfony\Bridge\Doctrine\Form\Type\EntityType; // Import EntityType
+
+class EvenementType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('nom', TextType::class, [
+                'constraints' => [
+                    new NotBlank(),
+                    new Length(['min' => 3, 'max' => 255]),
+                ],
+            ])
+            ->add('description', TextareaType::class, [
+                'constraints' => [
+                    new NotBlank(),
+                    new Length(['min' => 10]),
+                ],
+            ])
+            ->add('dateE', DateType::class, [
+                'constraints' => [
+                    new NotBlank(),
+                ],
+            ])
+            ->add('image', TextType::class, [
+                // Add constraints if needed for file uploads
+            ])
+            ->add('categorie', EntityType::class, [ // Change ChoiceType to EntityType
+                'class' => Categorie::class, // Specify the class of the entities to load
+                'choice_label' => 'nom', // Property of the entity to use as the choice label
+                'constraints' => [
+                    new NotBlank(),
+                ],
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Evenement::class,
+        ]);
+    }
+
+}
